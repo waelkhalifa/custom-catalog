83cf03d95cc1a21f080b570b0f5cb593f0c048ae
Wael Khalifa <wnkhalifa99@gmail.com>
2022-10-01 21:20:38 +0300

    moved

--- registration.php
+++ registration.php
@@ -1,10 +1,11 @@
 <?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 
-
-use \Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Wael_CustomCatalog',
     __DIR__
 );
